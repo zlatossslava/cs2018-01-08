139695c9d198d09e0504a476e205bf431f8221e4
Anastasiya Borovik <info.borovik@mail.ru>
2018-01-11 00:34:05 +0300

    TaskC3-   не могу понять причину ошибки в 21строке. ПРограмма компилируется с 1 ошибкой. Перепроверяю тесты, что делали в классе- они тоже с ошибкой выдаются, хотя в классе все работало

--- src/by/it/borovik/lesson02/TaskC1.java
+++ src/by/it/borovik/lesson02/TaskC1.java
@@ -1,5 +1,7 @@
 package by.it.borovik.lesson02;
 
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и выводит их сумму на экран в виде
 
@@ -9,10 +11,15 @@ package by.it.borovik.lesson02;
 Вывод:
 Sum = 26
 */
+
 class TaskC1 {
+    public static void main(String[] args) {
+        System.out.print("Введите клавиатурой число 34");
+        Scanner a = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.print("Введите клавиатурой число 26");
+        int b = scan.nextInt();
+        System.out.print("Сумма чисел 26 и 34 равна "+(a+b));
 
-
-
-
-
+    }
 }
