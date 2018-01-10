9212bd91defd54dcfd0c762a7376290a3da327cc
Anastasiya Borovik <info.borovik@mail.ru>
2018-01-10 12:19:14 +0300

    TaskA1 comolete

---
+++ src/by/it/borovik/lesson02/TaskA2.java
@@ -0,0 +1,20 @@
+package by.it.borovik.lesson02;
+
+/*
+напишите тело main программы которая выводит на экран 5 строк
+"Я начинаю изучать Java!"
+
+Для ручной проверки запустите программу Ctrl+Shift+F10
+Для автоматической проверки откройте и запустите класс Testing
+*/
+
+
+class TaskA2 {
+    public static void main(String[] args) {
+        String i = "Я начинаю изучать Java";
+        int y = 1 {
+            for (y = 0; y < 5; y++)
+                System.out.println(i);
+        }
+        }
+    }
