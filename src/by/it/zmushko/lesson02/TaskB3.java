d813e455b3e3113126eb67c5df38151218a81cf8
ZmushkoPavel <hunter19_87@mail.ru>
2018-01-12 04:10:42 +0300

    taskA1 complete taskA2 complete taskA3 complete

--- src/by/it/zmushko/lesson02/TaskB3.java
+++ src/by/it/zmushko/lesson02/TaskB3.java
@@ -10,15 +10,7 @@ package by.it.zmushko.lesson02;
 */        
 class TaskB3 {
         public static void main(String[] args) {
-//        String s = "C";
-//        System.out.println("Новым Годом");
 //        System.out.println("C Новым");
-//        System.out.println("Годом");
-//        System.out.print("C Новым");
-//        System.out.println("Годом");
-//        System.out.println(s);
-//        System.out.print("C Новым!");
-//        System.out.print(" ");
 //        System.out.println("Годом");
     }
 }
