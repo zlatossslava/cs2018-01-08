2fb6d34d64c7cf4d2c78323c00626ab0fab5fd58
shliapik <nat-kka@mail.ru>
2018-01-12 10:54:58 +0300

    Tasks B1-3, C1-2 have done

--- src/by/it/shliapik/lesson02/TaskC3.java
+++ src/by/it/shliapik/lesson02/TaskC3.java
@@ -1,5 +1,7 @@
 package by.it.shliapik.lesson02;
 
+import java.util.Scanner;
+
 /*
 Ускорение свободного падения на Земле и Марсе таково:
 Марс   3,86
@@ -30,5 +32,17 @@ package by.it.shliapik.lesson02;
 
 */
 class TaskC3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int weight=sc.nextInt();
+        private static double getWeight(int weight){
+            double a=weight/9.81*3.86;
+            return (double) a;
+        }
+
+        System.out.println(getWeight(a));
+    }
+
+}
 
 }
