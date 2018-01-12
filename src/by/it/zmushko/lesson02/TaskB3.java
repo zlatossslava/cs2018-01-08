1d698a67447aefe06d3289d709f0aff022266abe
ZmushkoPavel <hunter19_87@mail.ru>
2018-01-12 11:12:26 +0300

    taskA1 complete taskA2 complete taskA3 complete taskB1 complete taskB2 complete taskB3 complete

--- src/by/it/zmushko/lesson02/TaskB3.java
+++ src/by/it/zmushko/lesson02/TaskB3.java
@@ -13,8 +13,8 @@ class TaskB3 {
     public static void main(String[] args) {
 //        String s = "C";
 //        System.out.println("Новым Годом");
-        System.out.println("C Новым");
-        System.out.println("Годом");
+        String s = "C Новым ";
+        System.out.println(s + "Годом");
 //        System.out.print("C Новым");
 //        System.out.println("Годом");
 //        System.out.println(s);
