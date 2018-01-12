6312d668179c392574802814d6e80c2bb5ad8cee
komrikv <komrik1984@mail.ru>
2018-01-12 11:02:58 +0200

    TaskA TaskB commit

--- src/by/it/komar/lesson02/TaskB3.java
+++ src/by/it/komar/lesson02/TaskB3.java
@@ -10,15 +10,15 @@ package by.it.komar.lesson02;
 */        
 class TaskB3 {
         public static void main(String[] args) {
-//        String s = "C";
+//       String s = "C";
 //        System.out.println("Новым Годом");
 //        System.out.println("C Новым");
 //        System.out.println("Годом");
-//        System.out.print("C Новым");
-//        System.out.println("Годом");
+       System.out.print("C Новым");
+//       System.out.println("Годом");
 //        System.out.println(s);
 //        System.out.print("C Новым!");
-//        System.out.print(" ");
-//        System.out.println("Годом");
+       System.out.print(" ");
+        System.out.println("Годом");
     }
 }
